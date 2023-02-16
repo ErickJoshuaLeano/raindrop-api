175e8f5fb0abc13e96e15e420ceb65b9c946e47d
ErickJoshuaLeano <ejoshua.leano@gmail.com>
2023-02-16 22:24:31 +0800

    Added Swagger API Compatibility

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,14 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   // handle all user input validation globally
   app.useGlobalPipes(new ValidateInputPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
